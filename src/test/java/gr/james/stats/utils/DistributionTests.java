cfcf3404cd015ec792fb2894e8e46efc1eb9b176
Giorgos Stamatelatos <gstamatelat@gmail.com>
2021-02-24 20:14:50 +0200

    Add Distribution tests

---
+++ src/test/java/gr/james/stats/utils/DistributionTests.java
@@ -0,0 +1,20 @@
+package gr.james.stats.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DistributionTests {
+    /**
+     * Test the mode
+     */
+    @Test
+    public void modeTest() {
+        final Distribution d = new Distribution();
+        d.add(1.0);
+        d.add(2.0);
+        d.add(3.0);
+        d.add(3.0);
+        d.add(2.0);
+        Assert.assertEquals(2.0, d.mode(), 1e-4);
+    }
+}
