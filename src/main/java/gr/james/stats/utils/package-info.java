9aebcd31a03cce70ae5b0f43ffdfe0e5e414ac49
Giorgos Stamatelatos <gstamatelat@gmail.com>
2021-04-06 15:14:24 +0300

    Add Welford's algorithm on package-info

--- src/main/java/gr/james/stats/utils/package-info.java
+++ src/main/java/gr/james/stats/utils/package-info.java
@@ -4,6 +4,7 @@
  *     <li>{@link gr.james.stats.utils.Frequency}</li>
  *     <li>{@link gr.james.stats.utils.Distribution}</li>
  *     <li>{@link gr.james.stats.utils.Plotting}</li>
+ *     <li>{@link gr.james.stats.utils.WelfordVariance}</li>
  * </ul>
  */
 package gr.james.stats.utils;
